247be4dec9605210245648aec99df684125822ed
Christian Segura <ces1508@gmail.com>
2020-04-14 22:16:26 -0500

    Add category item

---
+++ src/components/CategoryItem/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './categoryItem.scss'
+
+const CategoryItem = props => {
+  const { category, type, handleClick, active } = props
+  let baseClass = 'category_item'
+  const isRestaurant = type === 'RESTAURANT'
+  if (!isRestaurant) baseClass += ' category_item--product'
+  if (isRestaurant) baseClass += ' category_item--restaurant'
+  if (active && isRestaurant) baseClass += ' category_item--active category_item--active_restaurant'
+  if (active && !isRestaurant) baseClass += ' category_item--active category_item--active_product'
+  return (
+    <div className={baseClass} onClick={handleClick}>
+      <img
+        className='category_item__image'
+        src={category.image}
+        alt={category.name} />
+      <div className='category_item__description '>
+        <p className='category_item__name'>{category.name}</p>
+        {/* <p className='category_item__quantity'>{quantity}</p> */}
+      </div>
+    </div>
+  )
+}
+
+CategoryItem.propTypes = {
+  category: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
+  }).isRequired,
+  active: PropTypes.bool,
+  handleClick: PropTypes.func,
+  type: PropTypes.oneOf(['PRODUCT', 'RESTAURANT']).isRequired
+}
+
+CategoryItem.defaultProps = {
+  type: 'PRODUCT'
+}
+
+export default CategoryItem
---
+++ src/stories/7-CategoryItem.stories.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import CategoryItem from '../components/CategoryItem'
+
+export default {
+  title: 'Category Item',
+  component: CategoryItem
+}
+
+const category = {
+  name: 'Servicios para Mascotas',
+  quantity: 30,
+  image: 'https://fusemall.s3.us-west-2.amazonaws.com/uploads/category/image/59/thumb_e4d90b0b-9d31-40f1-9efe-d600629a7ece.jpeg'
+}
+
+export const Basic = () => <div style={{ paddingLeft: '2em' }}><CategoryItem category={category} /></div>
+export const Active = () => <div style={{ paddingLeft: '2em' }}><CategoryItem category={category} active /></div>
+export const ClickToActive = () => {
+  const [active, setActive] = useState(false)
+  return (
+    <div style={{ paddingLeft: '2em' }}>
+      <CategoryItem
+        handleClick={() => setActive(!active)}
+        category={category}
+        active={active} />
+      </div>
+  )
+}
+
+
+export const TypeRestaurant = () => {
+  const [active, setActive] = useState(false)
+  return (
+    <div style={{ paddingLeft: '2em' }}>
+      <CategoryItem
+        handleClick={() => setActive(!active)}
+        category={category}
+        type='RESTAURANT'
+        active={active} />
+      </div>
+  )
+}
+
+export const TypeProduct = () => {
+  const [active, setActive] = useState(false)
+  return (
+    <div style={{ paddingLeft: '2em' }}>
+      <CategoryItem
+        handleClick={() => setActive(!active)}
+        category={category}
+        type='PRODUCT'
+        active={active} />
+      </div>
+  )
+}
